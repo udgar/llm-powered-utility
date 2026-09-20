1fa638866cffbcf16c09aa57442e1468db48066c
udgarkhanal <khanaludgar@gmail.com>
2026-09-20 13:19:24 +0545

    add system prompt by giving the role to LLM as SWIFT expert

--- src/main/java/com/llm/powered/app/service/LlmChatService.java
+++ src/main/java/com/llm/powered/app/service/LlmChatService.java
@@ -1,19 +1,32 @@
 package com.llm.powered.app.service;
 
 import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.io.Resource;
 import org.springframework.stereotype.Service;
 import reactor.core.publisher.Flux;
+
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
 
 @Service
 public class LlmChatService {
 
     private final ChatClient chatClient;
+    private final String SYSTEM_PROMPT;
 
-    public LlmChatService(ChatClient.Builder chatClientBuilder) {
+    public LlmChatService(ChatClient.Builder chatClientBuilder
+            , @Value("classpath:prompt.txt") Resource promptResource) throws IOException {
         this.chatClient = chatClientBuilder.build();
+        this.SYSTEM_PROMPT = systemPrompt(promptResource);
     }
 
     public Flux<String> response(String prompt) {
-        return chatClient.prompt().user(prompt).stream().content();
+        return chatClient.prompt().system(SYSTEM_PROMPT).user(prompt).stream().content();
+    }
+
+    private String systemPrompt(Resource promptResource) throws IOException {
+        assert promptResource != null;
+        return promptResource.getContentAsString(StandardCharsets.UTF_8);
     }
 }
