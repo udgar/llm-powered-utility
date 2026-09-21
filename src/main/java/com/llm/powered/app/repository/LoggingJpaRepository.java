d2024451692d6abb841cf02b600c8e936b186331
udgarkhanal <khanaludgar@gmail.com>
2026-09-21 18:41:04 +0545

    fix LoggingJpaRepository convention

--- src/main/java/com/llm/powered/app/repository/LoggingJpaRepository.java
+++ src/main/java/com/llm/powered/app/repository/LoggingJpaRepository.java
@@ -5,5 +5,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface LoggingRepository extends JpaRepository<Long, LoggingEntity> {
+public interface LoggingJpaRepository extends JpaRepository<LoggingEntity,Long> {
 }
